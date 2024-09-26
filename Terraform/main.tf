7870181a2cb0a6502db94285c8bdd11e54b1c96e
Sarthak Jain <90322470+sarthakjain2238@users.noreply.github.com>
2024-09-26 14:33:09 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "medusa_instance" {
 }
 
 resource "aws_security_group" "new_security_group" {
-  name        = "medusa_security_group_depkak"
+  name        = "medusa_security_group_soni"
   description = "Allow HTTP, HTTPS, SSH, and Medusa traffic"
   vpc_id      = "vpc-02aca3c29abc1cd43"  # Replace with your VPC ID
 }
