4863bf49ecd1ed679f50301955d6c94dc75fe991
Sarthak Jain <90322470+sarthakjain2238@users.noreply.github.com>
2024-09-26 11:59:37 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "medusa_instance" {
   ami           = "ami-0e86e20dae9224db8"  # Choose an appropriate AMI ID based on your region
-  instance_type = "t2.large"
+  instance_type = "t2.medium"
   key_name      = "new"
 
   tags = {
@@ -13,7 +13,7 @@ resource "aws_instance" "medusa_instance" {
 }
 
 resource "aws_security_group" "new_security_group" {
-  name        = "medusa_security_group_sarthak1"
+  name        = "medusa_security_group_sarthak2"
   description = "Allow HTTP, HTTPS, SSH, and Medusa traffic"
   vpc_id      = "vpc-02aca3c29abc1cd43"  # Replace with your VPC ID
 }
